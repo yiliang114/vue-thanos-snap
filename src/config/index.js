6944dbd8b04ee4b7f22976ba4356f34b6296b181
mrjzhang <mrjzhang@tencent.com>
2019-05-04 17:19:38 +0800

    rebuild

--- src/config/index.js
+++ src/config/index.js
@@ -23,76 +23,76 @@ export const heroes = [
     src: require("../assets/avatar/4.jpg"),
     show: true
   },
-  // {
-  //   id: 5,
-  //   name: "Thor",
-  //   src: require("../assets/avatar/5.jpg"),
-  //   show: true
-  // },
-  // {
-  //   id: 6,
-  //   name: "Hulk",
-  //   src: require("../assets/avatar/6.jpg"),
-  //   show: true
-  // },
-  // {
-  //   id: 7,
-  //   name: "Black Panther",
-  //   src: require("../assets/avatar/7.jpg"),
-  //   show: true
-  // },
-  // {
-  //   id: 8,
-  //   name: "Doctor Stranger",
-  //   src: require("../assets/avatar/8.jpg"),
-  //   show: true
-  // },
-  // {
-  //   id: 9,
-  //   name: "Clint",
-  //   src: require("../assets/avatar/Clint.jpeg"),
-  //   show: true
-  // },
-  // {
-  //   id: 10,
-  //   name: "Drax",
-  //   src: require("../assets/avatar/Drax.jpg"),
-  //   show: true
-  // },
-  // {
-  //   id: 11,
-  //   name: "Groot",
-  //   src: require("../assets/avatar/Groot.jpg"),
-  //   show: true
-  // },
-  // {
-  //   id: 12,
-  //   name: "Loki",
-  //   src: require("../assets/avatar/Loki.jpeg"),
-  //   show: true
-  // },
-  // {
-  //   id: 13,
-  //   name: "Winter",
-  //   src: require("../assets/avatar/Winter.jpg"),
-  //   show: true
-  // },
-  // {
-  //   id: 14,
-  //   name: "Mantis",
-  //   src: require("../assets/avatar/Mantis.jpg"),
-  //   show: true
-  // },
-  // {
-  //   id: 15,
-  //   name: "Nebula",
-  //   src: require("../assets/avatar/Nebula.jpg"),
-  //   show: true
-  // },
-  // {
-  //   id: 16,
-  //   name: "Vision",
-  //   src: require("../assets/avatar/Vision.jpg"),
-  //   show: true
-  // }
+  {
+    id: 5,
+    name: "Thor",
+    src: require("../assets/avatar/5.jpg"),
+    show: true
+  },
+  {
+    id: 6,
+    name: "Hulk",
+    src: require("../assets/avatar/6.jpg"),
+    show: true
+  },
+  {
+    id: 7,
+    name: "Black Panther",
+    src: require("../assets/avatar/7.jpg"),
+    show: true
+  },
+  {
+    id: 8,
+    name: "Doctor Stranger",
+    src: require("../assets/avatar/8.jpg"),
+    show: true
+  },
+  {
+    id: 9,
+    name: "Clint",
+    src: require("../assets/avatar/Clint.jpeg"),
+    show: true
+  },
+  {
+    id: 10,
+    name: "Drax",
+    src: require("../assets/avatar/Drax.jpg"),
+    show: true
+  },
+  {
+    id: 11,
+    name: "Groot",
+    src: require("../assets/avatar/Groot.jpg"),
+    show: true
+  },
+  {
+    id: 12,
+    name: "Loki",
+    src: require("../assets/avatar/Loki.jpeg"),
+    show: true
+  },
+  {
+    id: 13,
+    name: "Winter",
+    src: require("../assets/avatar/Winter.jpg"),
+    show: true
+  },
+  {
+    id: 14,
+    name: "Mantis",
+    src: require("../assets/avatar/Mantis.jpg"),
+    show: true
+  },
+  {
+    id: 15,
+    name: "Nebula",
+    src: require("../assets/avatar/Nebula.jpg"),
+    show: true
+  },
+  {
+    id: 16,
+    name: "Vision",
+    src: require("../assets/avatar/Vision.jpg"),
+    show: true
+  }
 ]
